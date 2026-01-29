6779cf23673aaf488cc51764450c69b75e078cd8
calvin <bamwood3@gmail.com>
2026-01-29 17:20:19 -0500

    firsty commit

---
+++ automate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Set variables
+LOG_DIR=/trading/logs
+ARCHIVE_DIR=~/trading/archive
+DATE=$(date +%Y-%m-%d)
+
+# Create archive folder if missing
+mkdir -p $ARCHIVE_DIR
+
+# Move and compress logs
+tar -czf $ARCHIVE_DIR/logs_$DATE.tar.gz $LOG_DIR/*.log
+
+
+echo "Logs archived and cleaned on $DATE"
